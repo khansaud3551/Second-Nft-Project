88c5ad5fe9612cefea5d265d10eef314f50da5ac
Saud Khan <khansaud3551@gmail.com>
2022-04-03 15:45:31 +0500

    #updted one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         </div>
       </div>
       <div className=" d-flex py-2 flex-column justify-content-center align-items-center bg__footer py-0">
-        <p className="m-0 ">2021 © All Rights Reserved</p>
+        <p className="m-0 ">2022 © All Rights Reserved</p>
         {/* <div className="mt-2 d-flex footer__bottom pb-2">
           <a className="m-0" style={{ fontSize: "15px" }} href="">
             Terms & Conditions
